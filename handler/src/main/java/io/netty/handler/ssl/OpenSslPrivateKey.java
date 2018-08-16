17fd36021f200a7458041357cf00e9bf413b1191
Norman Maurer <norman_maurer@apple.com>
2018-08-16 15:14:36 +0200

    Allow to load PrivateKey via OpenSSL Engine
    Motivation:
    
    OpenSSL itself has an abstraction which allows you to customize some things. For example it is possible to load the PrivateKey from the engine. We should support this.
    
    Modifications:
    
    Add two new static methods to OpenSslX509KeyManagerFactory which allow to create an OpenSslX509KeyManagerFactory that loads its PrivateKey via the OpenSSL Engine directly.
    
    Result:
    
    More flexible usage of OpenSSL possible

---
+++ handler/src/main/java/io/netty/handler/ssl/OpenSslPrivateKey.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2018 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.ssl;
+
+import io.netty.internal.tcnative.SSL;
+import io.netty.util.AbstractReferenceCounted;
+import io.netty.util.IllegalReferenceCountException;
+
+import javax.security.auth.Destroyable;
+import java.security.PrivateKey;
+
+final class OpenSslPrivateKey extends AbstractReferenceCounted implements PrivateKey {
+
+    private volatile long privateKey;
+
+    OpenSslPrivateKey(long privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    @Override
+    public String getAlgorithm() {
+        return null;
+    }
+
+    @Override
+    public String getFormat() {
+        return "native";
+    }
+
+    @Override
+    public byte[] getEncoded() {
+        return null;
+    }
+
+    /**
+     * Returns the pointer to the {@code EVP_PKEY}.
+     */
+    public long privateKeyAddress() {
+        if (refCnt() <= 0) {
+            throw new IllegalReferenceCountException();
+        }
+        return privateKey;
+    }
+
+    @Override
+    protected void deallocate() {
+        SSL.freePrivateKey(privateKey);
+        privateKey = 0;
+    }
+
+    @Override
+    public OpenSslPrivateKey retain() {
+        super.retain();
+        return this;
+    }
+
+    @Override
+    public OpenSslPrivateKey retain(int increment) {
+        super.retain(increment);
+        return this;
+    }
+
+    @Override
+    public OpenSslPrivateKey touch() {
+        super.touch();
+        return this;
+    }
+
+    @Override
+    public OpenSslPrivateKey touch(Object hint) {
+        return this;
+    }
+
+    /**
+     * NOTE: This is a JDK8 interface/method. Due to backwards compatibility
+     * reasons it's not possible to slap the {@code @Override} annotation onto
+     * this method.
+     *
+     * @see Destroyable#destroy()
+     */
+    public void destroy() {
+        release(refCnt());
+    }
+
+    /**
+     * NOTE: This is a JDK8 interface/method. Due to backwards compatibility
+     * reasons it's not possible to slap the {@code @Override} annotation onto
+     * this method.
+     *
+     * @see Destroyable#isDestroyed()
+     */
+    public boolean isDestroyed() {
+        return refCnt() == 0;
+    }
+
+    /**
+     * Convert to a {@link OpenSslKeyMaterial}. Reference count of both is shared.
+     */
+    OpenSslKeyMaterial toKeyMaterial(long certificateChain) {
+        return new OpenSslPrivateKeyMaterial(certificateChain);
+    }
+
+    private final class OpenSslPrivateKeyMaterial implements OpenSslKeyMaterial {
+
+        private volatile long certificateChain;
+
+        OpenSslPrivateKeyMaterial(long certificateChain) {
+            this.certificateChain = certificateChain;
+        }
+
+        @Override
+        public long certificateChainAddress() {
+            if (refCnt() <= 0) {
+                throw new IllegalReferenceCountException();
+            }
+            return certificateChain;
+        }
+
+        @Override
+        public long privateKeyAddress() {
+            return OpenSslPrivateKey.this.privateKeyAddress();
+        }
+
+        @Override
+        public OpenSslKeyMaterial retain() {
+            OpenSslPrivateKey.this.retain();
+            return this;
+        }
+
+        @Override
+        public OpenSslKeyMaterial retain(int increment) {
+            OpenSslPrivateKey.this.retain(increment);
+            return this;
+        }
+
+        @Override
+        public OpenSslKeyMaterial touch() {
+            OpenSslPrivateKey.this.touch();
+            return this;
+        }
+
+        @Override
+        public OpenSslKeyMaterial touch(Object hint) {
+            OpenSslPrivateKey.this.touch(hint);
+            return this;
+        }
+
+        @Override
+        public boolean release() {
+            if (OpenSslPrivateKey.this.release()) {
+                releaseChain();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public boolean release(int decrement) {
+            if (OpenSslPrivateKey.this.release(decrement)) {
+                releaseChain();
+                return true;
+            }
+            return false;
+        }
+
+        private void releaseChain() {
+            SSL.freeX509Chain(certificateChain);
+            certificateChain = 0;
+        }
+        @Override
+        public int refCnt() {
+            return OpenSslPrivateKey.this.refCnt();
+        }
+    }
+}
